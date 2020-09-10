73a82230d95ffc8978d32ce29d156bc1b783f82f
Daniel Nascimento <danielhn@gmail.com>
2020-09-09 20:31:33 -0400

    ESLint

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,52 +1,51 @@
-describe("Appointments", () => {
+describe('Appointments', () => {
   beforeEach(() => {
-   cy.request("GET", "/api/debug/reset");
- 
-   cy.visit("/");
- 
-   cy.contains("Monday");
-  });
- 
-  it("should book an interview", () => {
-   cy.get("[alt=Add]")
-    .first()
-    .click();
- 
-   cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
-   cy.get('[alt="Sylvia Palmer"]').click();
- 
-   cy.contains("Save")
-   .click();
- 
-   cy.contains(".appointment__card--show", "Lydia Miller-Jones");
-   cy.contains(".appointment__card--show", "Sylvia Palmer");
-  })
+    cy.request('GET', '/api/debug/reset');
 
-  it("should edit an interview", () => {
-    cy.get("[alt=Edit]")
+    cy.visit('/');
+
+    cy.contains('Monday');
+  });
+
+  it('should book an interview', () => {
+    cy.get('[alt=Add]')
+      .first()
+      .click();
+
+    cy.get('[data-testid=student-name-input]').type('Lydia Miller-Jones');
+    cy.get('[alt="Sylvia Palmer"]').click();
+
+    cy.contains('Save')
+      .click();
+
+    cy.contains('.appointment__card--show', 'Lydia Miller-Jones');
+    cy.contains('.appointment__card--show', 'Sylvia Palmer');
+  });
+
+  it('should edit an interview', () => {
+    cy.get('[alt=Edit]')
       .first()
       .click({ force: true });
-  
-    cy.get("[data-testid=student-name-input]").clear().type("Lydia Miller-Jones");
+
+    cy.get('[data-testid=student-name-input]').clear().type('Lydia Miller-Jones');
     cy.get("[alt='Tori Malcolm']").click();
-  
-    cy.contains("Save").click();
-  
-    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
-    cy.contains(".appointment__card--show", "Tori Malcolm");
+
+    cy.contains('Save').click();
+
+    cy.contains('.appointment__card--show', 'Lydia Miller-Jones');
+    cy.contains('.appointment__card--show', 'Tori Malcolm');
   });
 
-  it("should cancel an interview", () => {
-    cy.get("[alt=Delete]")
+  it('should cancel an interview', () => {
+    cy.get('[alt=Delete]')
       .click({ force: true });
-  
-    cy.contains("Confirm").click();
-  
-    cy.contains("DELETING").should("exist");
-    cy.contains("DELETING").should("not.exist");
-  
-    cy.contains(".appointment__card--show", "Archie Cohen")
-      .should("not.exist");
-  });
 
-})
+    cy.contains('Confirm').click();
+
+    cy.contains('DELETING').should('exist');
+    cy.contains('DELETING').should('not.exist');
+
+    cy.contains('.appointment__card--show', 'Archie Cohen')
+      .should('not.exist');
+  });
+});
